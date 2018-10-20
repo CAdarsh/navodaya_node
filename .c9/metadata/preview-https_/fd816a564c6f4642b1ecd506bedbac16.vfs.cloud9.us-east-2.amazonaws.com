c817b0a140e6ceef7d4cfc24569af7919cf49532
unknown <you@example.com>
2018-10-20 10:02:46 +0530

    first commit

---
+++ .c9/metadata/preview-https_/fd816a564c6f4642b1ecd506bedbac16.vfs.cloud9.us-east-2.amazonaws.com
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://fd816a564c6f4642b1ecd506bedbac16.vfs.cloud9.us-east-2.amazonaws.com","undoManager":{"mark":-1,"position":-1,"stack":[]},"preview":{"path":"https://fd816a564c6f4642b1ecd506bedbac16.vfs.cloud9.us-east-2.amazonaws.com","previewer":"preview.browser"},"timestamp":1539924263706,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var AWS = require('aws-sdk');
+var app = express();
+app.use(express.static('./public'));
+var value = "Adarsh";
+app.get('/',(req,res) => {
+    res.render('index.ejs',{value:value});
+});
+app.get("/photo_gallery",(req,res) => {
+    res.render('photoG.ejs');
+});
+app.get("/video_gallery",(req,res) => {
+    res.render('videoG.ejs');
+});
+
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+  //call this app from https://<workspace name>-<user name>.c9users.io
+});
